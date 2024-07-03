9b885df2f252d69af77483b7f7ba074c6d6df49e
Anuj-Wanjari <anujwanjari5@gmail.com>
2024-07-03 11:25:05 +0530

    middleware

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// import express from 'express';
-// const app = express();
-
-// let courses = [
-//     { id: 1, name: "course1" },
-//     { id: 2, name: "course2" },
-//     { id: 3, name: "course3" },
-// ];
-
-// // Route for the root URL
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the courses API');
-// });
-
-// app.get('/courses', (req, res) => {
-//     res.json(courses);
-// });
-
-// app.listen(3000, () => {
-//     console.log("server started on http://localhost:3000");
-// });
-
-
 import express from 'express';
 const app = express();
 
@@ -31,6 +8,19 @@ let courses = [
     { id: 2, name: "course2" },
     { id: 3, name: "course3" },
 ];
+
+// Middleware function
+const logRequestDetails = (req, res, next) => {
+    const method = req.method;
+    const ip = req.ip;
+    const hostname = req.hostname;
+    const date = new Date().toISOString();
+    console.log(`Method: ${method}, IP: ${ip}, Hostname: ${hostname}, Date: ${date}`);
+    next();
+};
+
+// Applying middleware to all routes
+app.use(logRequestDetails);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the courses API');
